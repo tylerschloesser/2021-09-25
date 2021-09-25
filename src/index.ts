83d2cc9c6006c6bb59b367ba0020e0a675ec7bcd
Tyler Schloesser <tschloes@amazon.com>
2021-09-25 10:05:55 -0700

    draw circle

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,27 @@ interface Viewport {
   h: number
 }
 
+function drawCircle({ context }: Viewport, c: Vec2, r: number) {
+  context.beginPath()
+  context.arc(c[0], c[1], r, 0, 2 * Math.PI)
+  context.fill()
+}
+
 function draw(viewport: Viewport, state: State) {
 
   const { context, w, h } = viewport
 
+  context.clearRect(0, 0, w, h)
   context.fillStyle = '#333'
   context.fillRect(0, 0, w, h)
+
+  
+  context.fillStyle = 'white'
+
+  context.translate(w/2, h/2)
+  drawCircle(viewport, [0, 0], 10)
+  context.resetTransform()
+
 
 }
 
@@ -30,6 +45,7 @@ async function main() {
   const w = canvas.width
   const h = canvas.height
 
+  console.log({ w, h })
 
 
   const state: State = {
