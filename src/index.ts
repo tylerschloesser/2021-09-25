d028523f4bffba388fe4fa4d7e679ab02f2596fb
Tyler Schloesser <tschloes@amazon.com>
2021-09-25 08:46:44 -0700

    spinning letters

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,52 @@ import random from 'lodash/random'
 
 function main() {
   const name = document.getElementById('name')!
-  'Tyler Schloesser'.split('').forEach((c) => {
+
+  const letters = 'Tyler Schloesser'.split('').map((c) => {
     const span = document.createElement('span')
+    span.classList.add('letter')
     span.innerText = c
     span.dataset.dx = random(100).toString()
     span.dataset.dy = random(100).toString()
     span.dataset.dr = random(100).toString()
 
     name.appendChild(span)
+
+    return span
   })
 
   // fixes some weird ass chrome bug.
   // really, try removing this.
   document.getElementById('name-container')!.style.height = '100%'
+
+  const start = performance.now()
+  const duration = 1000
+
+  const shift = (timestamp: number) => {
+    const elapsed = timestamp - start
+    let dt = elapsed / duration
+
+    if (dt >= 1) {
+      letters.forEach((span) => {
+        span.style.transform = ''
+      })
+      return
+    }
+
+    const scale = 1 - dt
+    letters.forEach((span) => {
+      const dx = parseInt(span.dataset.dx!)
+      const dy = parseInt(span.dataset.dy!)
+      const dr = parseInt(span.dataset.dr!)
+
+      span.style.transform = `translate(${dx * scale}px, ${
+        dy * scale
+      }px) rotate(${dr * scale * Math.PI * 2}rad)`
+    })
+
+    window.requestAnimationFrame(shift)
+  }
+  window.requestAnimationFrame(shift)
 }
 
 main()
