f5f3de8960494e5f1318854e629c2e5bb7f8a8bf
Tyler Schloesser <tschloes@amazon.com>
2021-09-25 08:53:29 -0700

    fix whitespace

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ function main() {
   const letters = 'Tyler Schloesser'.split('').map((c) => {
     const span = document.createElement('span')
     span.classList.add('letter')
-    span.innerText = c
+    span.innerHTML = c === ' ' ? '&nbsp;' : c
     span.dataset.dx = random(100).toString()
     span.dataset.dy = random(100).toString()
     span.dataset.dr = random(100).toString()
