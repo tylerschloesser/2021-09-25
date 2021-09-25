9cf12e11663ae93271f9116cb05e8f7c9270d90d
Tyler Schloesser <tschloes@amazon.com>
2021-09-25 08:01:59 -0700

    add name

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 function main() {
   document.addEventListener('scroll', () => {
-    console.log(window.scrollY)
+    //console.log(window.scrollY)
   })
+
+  const name = 'Tyler Schloesser'
+  const nameContainer = document.createElement('div')
+
+  name.split('').forEach((c) => {
+    const span = document.createElement('span')
+    span.innerText = c
+    nameContainer.appendChild(span)
+  })
+
+  const root = document.getElementById('root')!
+  root.appendChild(nameContainer)
 }
 
 main()
