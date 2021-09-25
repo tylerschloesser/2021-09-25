0f858139a3fd3900ec2036ca90e9b326a229f461
Tyler Schloesser <tschloes@amazon.com>
2021-09-25 10:01:57 -0700

    initial loop

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 
 
+type Vec2 = [number, number]
+
+interface State {
+  p: Vec2
+}
+
+interface Viewport {
+  context: CanvasRenderingContext2D
+  w: number
+  h: number
+}
+
+function draw(viewport: Viewport, state: State) {
+
+  const { context, w, h } = viewport
+
+  context.fillStyle = '#333'
+  context.fillRect(0, 0, w, h)
+
+}
+
 async function main() {
 
   const canvas = document.querySelector('canvas')!
@@ -9,9 +30,19 @@ async function main() {
   const w = canvas.width
   const h = canvas.height
 
-  context.fillStyle = '#333'
-  context.fillRect(0, 0, w, h)
 
+
+  const state: State = {
+    p: [0, 0],
+  }
+
+  const viewport: Viewport = { context, w, h }
+
+  const game = (timestamp: number) => {
+    draw(viewport, state)
+    window.requestAnimationFrame(game)
+  }
+  window.requestAnimationFrame(game)
 }
 
 main()
