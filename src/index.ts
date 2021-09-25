e0a014353f25938889e92587f88be7997ee8eb18
Tyler Schloesser <tschloes@amazon.com>
2021-09-25 09:57:15 -0700

    clear canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 
+
+async function main() {
+
+  const canvas = document.querySelector('canvas')!
+  const context = canvas.getContext('2d')!
+
+
+  const w = canvas.width
+  const h = canvas.height
+
+  context.fillStyle = '#333'
+  context.fillRect(0, 0, w, h)
+
+}
+
+main()
